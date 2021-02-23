e1eb7b681a62f76327082a8958c90462b21ab6ec
glen.nilsen@microchip.com <glen.nilsen@microchip.com>
2021-02-23 09:55:03 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Q43-Hello.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Q43-Hello.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=q43-hello.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/q43-hello.tar
